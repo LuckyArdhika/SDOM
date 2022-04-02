ff99acd3030343559455812788191eda864a0d55
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2022-04-02 07:17:13 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// SDOM Developed by Lucky Ardhika and comunity // 2022
+// Please dont edit this credit section
+// You can contrib for Cost Less Software
+
+get = (ind) => {
+  if (ind.slice(0, 1) === "."){ // class
+    const elemn = document.querySelector(ind);
+    return elemn;
+  } else if (ind.slice(0, 1) === "#"){ // id
+    const elemn = document.getElementById(ind);
+    return elemn;
+  } else if (ind.slice(0, 1) === "_"){ // name
+    const elemn = documnet.getElementById(ind);
+    return elemn;
+  } else { // any?
+    const elemn = document.querySelector(ind);
+    return elemn;
+};
+ 
+// unfinished
